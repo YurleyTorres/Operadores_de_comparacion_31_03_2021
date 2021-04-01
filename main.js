1e693df374986c6bf115dcb9782510c7cc2e829a
YurleyTorres <marly.torres.silva@gmail.com>
2021-03-31 20:43:01 -0500

    30-03-2021 Operadores

---
+++ main.js
@@ -0,0 +1,29 @@
+// let a = 10;
+// let b = '10';
+// console.warn(a, typeof(a));
+// console.warn(b, typeof(b));
+// consola . advertir ( "Menor" ) ;
+// consola . log ( a  <  b ) ;  // Operador de menor
+// consola . advertir ( "Menor o igual" ) ;
+// consola . log ( a  <=  b ) ;  // Operador de menor o igual
+// consola . advertir ( "Alcalde" ) ;
+// consola . log ( a  >  b ) ;  // Operador de mayor
+// consola . advertir ( "Mayor o igual" ) ;
+// consola . log ( a  > =  b ) ;  // Operador de mayor o igual
+
+
+// let a = 5;
+// let b = '5';
+// console.warn(a, typeof(a));
+// console.warn(b, typeof(b));
+// console.log(a != b); //Operador diferente 'Compara si el dato NO es igual'
+// console.log(a !== b.toLowerCase()); //Operador de NO identico 'Compra si el dato y el tipo de dato no son igual e identico'
+
+
+// Operador de igualdad
+// let a = 'hola';
+// let b = 'HOLA';
+// console.warn(a, typeof(a));
+// console.warn(b, typeof(b));
+// console.log(a == b); //Operador de igualdad 'Compara si el dato es igual'
+// console.log(a === b.toLowerCase()); //Operador de identico 'Compra si el dato y el tipo de dato es igual e identico'
